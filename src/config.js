4d62fd6c97f7b138fad23424bd40a15c3cf8a328
Jaime-MV <jaime.a.matamoros@gmail.com>
2026-02-17 22:43:09 -0600

    Conexion a render

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Para test localmente:
-export const API_URL = "http://localhost:8080/api";
+//export const API_URL = "http://localhost:8080/api";
 
 // Para test en Render:
-//export const API_URL = "https://api-platmod.onrender.com/api";
+export const API_URL = "https://api-platmod.onrender.com/api";
